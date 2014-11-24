97de2bf7ec67d60b9a82ea904cc254cb0fea0bf9
Michael Cohen <mcohen01@gmail.com>
2014-11-23 16:32:23 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### Getting and Cleaning Data
+
+#### Course Project
+
+
+#### Running the script
+
+git clone this repository and cd into the directory
+
+Download the data set from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+Unzip the file
+
+Place all of the .txt files in the same directory as run_analysis.R
+
+Run run_analysis.R
+
+run_analysis.R will generate tidy.cvs 
---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+library(reshape2)
+
+#merge training and test sets
+
+subjects_train <- read.table("subject_train.txt")
+subjects_test <- read.table("subject_test.txt")
+X_train <- read.table("X_train.txt")
+X_test <- read.table("X_test.txt")
+y_train <- read.table("y_train.txt")
+y_test <- read.table("y_test.txt")
+
+# add feature names
+features <- read.table("features.txt")
+names(X_train) <- features$V2
+names(X_test) <- features$V2
+
+# merge sets
+train <- cbind(subjects_train, y_train, X_train)
+test <- cbind(subjects_test, y_test, X_test)
+combined <- rbind(train, test)
+
+# mean and standard deviation 
+musd <- grepl("mean\\(\\)", names(combined)) | grepl("std\\(\\)", names(combined))
+
+# remove others
+combined <- combined[, musd]
+
+
+# factorize activity
+labs = c("Walking", "Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying")
+combined$activity <- factor(combined$activity, labels = labs)
+
+# TIDY!
+melted <- melt(combined, id = c("subjectID", "activity"))
+tidy <- dcast(melted, subjectID + activity ~ variable, mean)
+
+# write it out 
+write.csv(tidy, "tidy.csv", row.names=FALSE)
