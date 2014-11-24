f8482fb9fab31438071ed67881c961f2ba0866df
Michael Cohen <mcohen01@gmail.com>
2014-11-23 16:33:57 -0800

    first commit

--- run_analysis.R
+++ run_analysis.R
@@ -9,6 +9,9 @@ X_test <- read.table("X_test.txt")
 y_train <- read.table("y_train.txt")
 y_test <- read.table("y_test.txt")
 
+names(subjects_train) <- "sid"
+names(subjects_test) <- "sid"
+
 # add feature names
 features <- read.table("features.txt")
 names(X_train) <- features$V2
@@ -31,7 +34,7 @@ labs = c("Walking", "Walking Upstairs", "Walking Downstairs", "Sitting", "Standi
 combined$activity <- factor(combined$activity, labels = labs)
 
 # TIDY!
-melted <- melt(combined, id = c("subjectID", "activity"))
+melted <- melt(combined, id = c("sid", "activity"))
 tidy <- dcast(melted, subjectID + activity ~ variable, mean)
 
 # write it out 
